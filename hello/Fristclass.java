d3be7166ad6110a781c9febc0bfb64eb972f2cb8
Lighting-Ari <roydavil@gmail.com>
2025-07-01 16:27:18 +0530

    Create Fristclass.java

---
+++ hello/Fristclass.java
@@ -0,0 +1,14 @@
+package Mylearning_Java.hello;
+
+public class Fristclass {
+
+    public static void main(String[] args) {
+        System.out.print("Hello Tim");
+
+        boolean  isAlien= true;
+        if (isAlien == false) {
+            System.out.println("I'm not an alien");
+        }
+
+    }
+}
