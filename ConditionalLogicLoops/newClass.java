c21534889fdcfbf2935d1f971bb4ae48ca4bb032
Lighting-Ari <roydavil@gmail.com>
2025-07-27 13:07:35 +0530

    Create newClass.java

---
+++ ConditionalLogicLoops/newClass.java
@@ -0,0 +1,27 @@
+package Mylearning_Java.ConditionalLogicLoops;
+
+public class newClass {
+     public static void main (String[] args) {
+     char natoAlphabet = 'g';
+
+          switch (natoAlphabet){
+               case 'a': System.out.println("A = Able");
+                    break;
+               case 'b': System.out.println("B = Baker");
+                    break;
+               case 'c': System.out.println("C = Charlie");
+                    break;
+               case 'd': System.out.println("D = Dog");
+                    break;
+               case 'e': System.out.println("E = Easy");
+                    break;
+               default:
+                    System.out.println("The latter "+ natoAlphabet +" is not found");
+                    break;
+
+          }
+
+     }
+
+
+}
